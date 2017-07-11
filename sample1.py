8bd754ab47530ee5ccfbeaf854b975113c8e599f
Brosseau Valentin <c4software@gmail.com>
2017-07-11 17:27:42 +0200

    Create sample1.py

---
+++ sample1.py
@@ -0,0 +1,39 @@
+import io
+import picamera
+import cv2
+import numpy
+import time
+
+while True:
+    stream = io.BytesIO()
+
+    #Get the picture (low resolution, so it should be quite fast)
+    #Here you can also specify other parameters (e.g.:rotate the image)
+    with picamera.PiCamera() as camera:
+        camera.resolution = (320, 240)
+        camera.rotation = 270
+        camera.capture(stream, format='jpeg')
+
+    #Convert the picture into a numpy array
+    buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
+
+    #Now creates an OpenCV image
+    image = cv2.imdecode(buff, 1)
+
+    #Load a cascade file for detecting faces
+    face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
+
+    #Convert to grayscale
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    #Look for faces in the image using the loaded cascade file
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+    if len(faces) > 0:
+        print ("Face detected")
+        #Draw a rectangle around every found face
+        for (x,y,w,h) in faces:
+            cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
+
+        #Save the result image
+        cv2.imwrite('result_{0}.jpg'.format(time.time()),image)
